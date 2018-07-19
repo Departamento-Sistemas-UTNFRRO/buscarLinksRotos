e745f9cce931a0c0f541e9a449ddf05d9a39237e
Jose Rostagno <joserostagno@hotmail.com>
2018-07-19 19:59:20 -0300

    Reutilizar clases en el script

--- buscarLinksRotos.py
+++ buscarLinksRotos.py
@@ -15,102 +15,15 @@
 #    along with buscarLinksRotos; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-
-import urllib.request
-import urllib.parse
-import bs4
 import datetime
-import locale
-from googlesearch import search
 import TFIDF
 import time
+from googlesearch import search
 from pyfbutils.DataSetCSV import DataSetCSV
 from pyfbutils.PostFacebook import PostFacebook
-
-
-dominiosMapeados = {
-    'a.ln.com.ar': 'www.lanacion.com.ar',
-    'clar.in': 'www.clarin.com',
-    'mundial-brasil-2014.clarin.com': 'www.clarin.com',
-    'arq.clarin.com': 'www.clarin.com',
-    'deporteshd.clarin.com': 'www.clarin.com'
-}
-
-
-def alargar_url(req):
-    try:
-        urlResuelta = urllib.request.urlopen(req)
-        return urlResuelta.url
-    except Exception as ex:
-        print("ERROR" + str(ex))
-    return None
-
-
-def ObtenerDominioUrl(urlLink):
-    '''Devuelve el dominio del link'''
-    urlTraducida = urllib.parse.urlparse(urlLink)
-    return '{uri.netloc}'.format(uri=urlTraducida)
-
-
-def ObtenerDominioUrlMapeado(urlLink):
-    dominio = ObtenerDominioUrl(urlLink)
-    for viejo, nuevo in dominiosMapeados.items():
-        if(viejo in dominio):
-            dominio = dominio.replace(viejo, nuevo)
-    return dominio
-
-
-def getFechaNacion(soup):
-    fechaNacion = "FECHA NO ENCONTRADA"
-    if (soup is None):
-        return fechaNacion
-
-    try:
-        for tag in soup.find_all("meta"):
-            if tag.get("itemprop", None) == "datePublished":
-                return tag.get("content", None)
-        contenedor = soup.find(class_='fecha')
-        if(contenedor is not None):
-            fechaCompleta = contenedor.getText()
-            fechaCompleta = fechaCompleta.replace('\xa0', '')
-            fechaCompleta = fechaCompleta.replace('de', '')
-            fechaCompleta = fechaCompleta.replace('•', '')
-            fechaCompleta = fechaCompleta.replace('  ', ' ')
-            fechaCompleta = fechaCompleta.replace('  ', ' ')
-            fechaCompleta = fechaCompleta.strip()
-            locale.setlocale(locale.LC_TIME, 'es_AR')
-            if('•' in contenedor.getText()):
-                fechaCompleta = datetime.datetime.strptime(
-                    fechaCompleta, '%d %B %Y %H:%M')
-            else:
-                fechaCompleta = datetime.datetime.strptime(
-                    fechaCompleta, '%d %B %Y')
-            fechaNacion = fechaCompleta.strftime('%Y-%m-%d %H:%M:%S')
-    except Exception as ex:
-        print("ERROR" + str(ex))
-    return fechaNacion
-
-
-def getFechaClarin(soup):
-    fechaNacion = "FECHA NO ENCONTRADA"
-    if (soup is None):
-        return fechaNacion
-
-    for tag in soup.find_all("meta"):
-        if tag.get("itemprop", None) == "datePublished":
-            return tag.get("content", None)
-    return "FECHA NO ENCONTRADA"
-
-
-def getHtmlSoup(req):
-    try:
-        resp = urllib.request.urlopen(req)
-        html = resp.read()
-        soup = bs4.BeautifulSoup(html, 'html.parser')
-        return soup
-    except Exception as ex:
-        print("ERROR" + str(ex))
-        return None
+from pyfbutils.Link import Link
+from pyfbutils.ClarinPost import ClarinPost
+from pyfbutils.NacionPost import NacionPost
 
 
 def buscarLinksEnGoogle(datasetCSV):
@@ -120,30 +33,26 @@ def buscarLinksEnGoogle(datasetCSV):
         try:
             print(i)
             post_link = posts[i][1]
-            postFacebook = PostFacebook(post_link)
             link_url = posts[i][2]
             print(link_url)
-            # FIXME: incorporar en clase post
-            post_fecha = convertirTextoAFecha(posts[i][3])
 
+            link = Link(link_url)
+            print(link.linkDomain)
+            if link.esLinkAOmitir():
+                print('Omitiendo')
+                posts[i].append("LINK NULL")
+                posts[i].append("LINK NULL")
+                posts[i].append("LINK NULL")
+                continue
+
+            postFacebook = PostFacebook(post_link)
             datosPost = postFacebook.getInfoPostFacebook()
             titulo_post = datosPost[0]
             posts[i].append(titulo_post)
             posts[i].append(datosPost[1])
-            print(datosPost[0])
+            print(titulo_post)
 
-            if('youtu.be' in link_url or 'tapas.clarin.com' in link_url):
-                print('Omitiendo')
-                posts[i].append("LINK NULL")
-                continue
-
-            # if(titulo_post == ""):
-            #    print('Omitiendo')
-            #    posts[i].append("LINK Borrado")
-            #    continue
-
-            dominio = ObtenerDominioUrlMapeado(link_url)
-            print(dominio)
+            post_fecha = convertirTextoAFecha(posts[i][3])
 
             # Buscar el link en base a los datos que tengo
             # texto del post
@@ -152,25 +61,21 @@ def buscarLinksEnGoogle(datasetCSV):
             linkMismoDominio = []
 
             # en caso que funcione el link lo agrego directamente
-            try:
-                req = urllib.request.Request(link_url)
-                url_larga = alargar_url(req)
-                if (url_larga is not None):
-                    linkMismoDominio.append(url_larga)
-            except Exception:
-                pass
+            if (link.linkReal is not None):
+                linkMismoDominio.append(link.linkReal)
 
-            texto_a_buscar = titulo_post.replace('"', '') + " " + dominio
+            texto_a_buscar = titulo_post.replace('"', '') + " " + link.linkDomain
             for url in search(texto_a_buscar, tld='com.ar', lang='es', stop=5):
                 print(url)
-                if(dominio in url):
-                    req = urllib.request.Request(url)
-                    soup = getHtmlSoup(req)
+                if(link.linkDomain in url):
+                    # FIXME: aca puedo usar la clase link de nuevo??
                     if('clarin' in url):
-                        fecha_portal = getFechaClarin(soup)
+                        postPortal = ClarinPost(link)
+                        fecha_portal = postPortal.getFecha()
                     else:
                         if ('nacion' in url):
-                            fecha_portal = getFechaNacion(soup)
+                            postPortal = NacionPost(link)
+                            fecha_portal = postPortal.getFecha()
                         else:
                             continue
 
@@ -178,8 +83,7 @@ def buscarLinksEnGoogle(datasetCSV):
                         continue
 
                     fecha_portal = datetime.datetime.strptime(
-                        fecha_portal, '%Y-%m-%d %H:%M:%S')
-                    fecha_portal = fecha_portal.date()
+                        fecha_portal, '%Y-%m-%d %H:%M:%S').date()
                     if(fecha_portal <= post_fecha):
                         linkMismoDominio.append(url)
 
